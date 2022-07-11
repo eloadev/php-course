41834ffd4a87b83157c94abb47c746de5f68b8cb
Elo Mello <eloamello126@gmail.com.br>
2022-07-11 02:16:55 -0300

    first commit

---
+++ exercicio.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/main.css">
+    <link rel="stylesheet" href="assets/css/exercicio.css">
+    <script src="https://kit.fontawesome.com/05ccc3741f.js" crossorigin="anonymous"></script>
+    <title>Curso PHP</title>
+</head>
+<body class="exercicio">
+    <header class="cabecalho">
+        <h1>Curso PHP</h1>
+        <h2>Visualizacao dos exercícios</h2>
+    </header>
+    <nav class="navegacao">
+        <a href="#" class="green">Sem formatacao</a>
+        <a href="index.php" class="red">Voltar</a>
+    </nav>
+    <main class="principal">
+        <div class="conteudo">
+        </div>
+    </main>
+    <footer class="rodape">
+        <p>Eloá Mello © <?= date('Y');?></p>
+    </footer>
+</body>
+</html>
