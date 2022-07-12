38aace291353f2a88923966124c8457657e91d95
Elo Mello <eloamello126@gmail.com.br>
2022-07-12 01:00:07 -0300

    adding new exercises to the modules

--- exercicio.php
+++ exercicio.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -9,21 +10,27 @@
     <script src="https://kit.fontawesome.com/05ccc3741f.js" crossorigin="anonymous"></script>
     <title>Curso PHP</title>
 </head>
+
 <body class="exercicio">
     <header class="cabecalho">
         <h1>Curso PHP</h1>
-        <h2>Visualizacao dos exercícios</h2>
+        <h2>Visualização dos exercícios</h2>
     </header>
     <nav class="navegacao">
-        <a href="#" class="green">Sem formatacao</a>
+        <a href="" onClick="window.location.reload();" class="yellow">Refresh</a>
+        <a href=<?= "{$_GET["dir"]}/{$_GET["file"]}.php" ?> class="green">Sem formatacao</a>
         <a href="index.php" class="red">Voltar</a>
     </nav>
     <main class="principal">
         <div class="conteudo">
+            <?php
+                include(__DIR__ . "/{$_GET['dir']}/{$_GET['file']}.php");
+            ?>
         </div>
     </main>
     <footer class="rodape">
-        <p>Eloá Mello © <?= date('Y');?></p>
+        <p>Eloá Mello © <?= date('Y'); ?></p>
     </footer>
 </body>
+
 </html>
