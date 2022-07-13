9f36667bd427158e9dfdd1d9314a3d10025e8598
Elo Mello <eloamello126@gmail.com.br>
2022-07-12 22:16:25 -0300

    new number exercises

---
+++ tipos/desafio_precedencia.php
@@ -0,0 +1,4 @@
+<div class="titulo">Desafio Precedência</div>
+
+<?php
+    
---
+++ tipos/float.php
@@ -0,0 +1,15 @@
+<div class="titulo">Tipo Float</div>
+
+<?php
+    echo 1.1, '<br>';
+
+    var_dump(1.1);
+    echo '<br>';
+
+    echo PHP_FLOAT_MAX, '<br>';
+    echo PHP_FLOAT_MIN, '<br>';
+
+    echo 1.2e3, '<br>'; // 1.2 vezes tres zeros = 1200
+    echo 13E-3, '<br>'; // 0.013 (casas mudando de posicao)
+
+    
